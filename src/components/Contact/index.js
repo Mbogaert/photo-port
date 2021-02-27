394d150fbfbb3d270c3dff1ec780d412145b8beb
Mathew B <bogaert@email.arizona.edu>
2021-02-26 19:27:01 -0700

    form working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
     // JSX
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
-                // name input
+            <form id="contact-form" onSubmit={handleSubmit} >
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" onChange={handleChange} name="name" defaultValue={name} />
                 </div>
-                // email input
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="text" name="email" />
+                    <input type="email" onChange={handleChange} name="email" defaultValue={email} />
                 </div>
-                // message text area
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" row="5" />
+                    <textarea name="message" row="5" onChange={handleChange} defaultValue={message} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
